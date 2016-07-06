ee5c6e4cc153644598aa0a789456952c1707e5ae
Alice Lubic <lubicalice@gmail.com>
2016-07-06 13:11:39 -0400

    started a new project file because something keeps happening in R.Java

---
+++ minimaList/app/src/main/java/com/example/owlslubic/minimalist/ViewListActivity.java
@@ -0,0 +1,13 @@
+package com.example.owlslubic.minimalist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_list);
+    }
+}
